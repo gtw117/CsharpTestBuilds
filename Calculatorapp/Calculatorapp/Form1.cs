9293f46793d76e99362574890d80744c1488a757
gtw117 <wilborn.gt@gmail.com>
2017-08-07 13:23:59 -0500

    add functionality to operations_buttons

--- Calculatorapp/Calculatorapp/Form1.cs
+++ Calculatorapp/Calculatorapp/Form1.cs
@@ -12,19 +12,48 @@ namespace Calculatorapp
 {
     public partial class Form1 : Form
     {
+        Double value = 0;
+        String operation = "";
+        bool operation_pressed = false;
+
+
         public Form1()
         {
             InitializeComponent();
         }
+
 
         private void Form1_Load(object sender, EventArgs e)
         {
 
         }
 
-        private void button7_Click(object sender, EventArgs e)
+        
+        private void button_Click(object sender, EventArgs e)
         {
-            result.Text = result.Text + "1";
+            if (result.Text == "0")
+                result.Clear();
+
+            Button b = (Button)sender;
+            result.Text = result.Text + b.Text;
         }
+
+
+        private void button18_Click(object sender, EventArgs e)
+        {
+            result.Text = "0";
+        }
+
+
+        private void operator_click(object sender, EventArgs e)
+        {
+            Button b = (Button)sender;
+            operation = b.Text;
+            value = Double.Parse(result.Text);
+            operation_pressed = true;
+
+        }
+
+
     }
 }
